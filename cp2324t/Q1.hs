43d076d09a691a9a3eab53f7eeaf2d4402eab500
a97455 <a97455@alunos.uminho.pt>
2024-01-09 19:53:52 +0000

    q1

--- cp2324t/Q1.hs
+++ cp2324t/Q1.hs
@@ -1,13 +1,15 @@
 import Data.List
-import List
 import Cp
+import List (outList)
+
 
 rotl :: [[a]]->[[a]]
 rotl = transpose . map reverse
 
-matrot :: Eq a => [[a]] -> [a]
-matrot = cataList (either nil conc)
+
+{- matrot2 :: Eq a => [[a]] -> [a]
+matrot2 [] = []
+matrot2 (h:t) = conc(h, matrot2 (rotl t)) -}
 
 matrot2 :: Eq a => [[a]] -> [a]
-matrot2 [] = []
-matrot2 (h:t) = h ++ matrot2 (rotl t) 
+matrot2 = either nil (conc . (id >< matrot2).(id><rotl)) . outList
