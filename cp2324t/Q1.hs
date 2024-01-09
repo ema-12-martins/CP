bcee795f0132427e4c511ef2f940d8790d15657c
ema-12-martins <a97678@alunos.uminho.pt>
2024-01-09 15:59:39 +0000

    Nao deu

--- cp2324t/Q1.hs
+++ cp2324t/Q1.hs
@@ -5,7 +5,9 @@ import Cp
 rotl :: [[a]]->[[a]]
 rotl = transpose . map reverse
 
-recList  f   = id -|- id >< rotl
-
 matrot :: Eq a => [[a]] -> [a]
-matrot = cataList  (either nil conc)
+matrot = cataList (either nil conc)
+
+matrot2 :: Eq a => [[a]] -> [a]
+matrot2 [] = []
+matrot2 (h:t) = h ++ matrot2 (rotl t) 
