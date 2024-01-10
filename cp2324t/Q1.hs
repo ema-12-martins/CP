d2f8a0a6b7104477cbd5c156e6e61602453b2c94
a97455 <a97455@alunos.uminho.pt>
2024-01-10 19:41:21 +0000

    q2 quase

--- cp2324t/Q1.hs
+++ cp2324t/Q1.hs
@@ -2,16 +2,14 @@ module Q1 where
     
 import Data.List
 import Cp
-import List (outList)
-
-
-rotl :: [[a]]->[[a]]
-rotl = transpose . map reverse
-
+import List
 
 {- matrot :: Eq a => [[a]] -> [a]
 matrot [] = []
 matrot (h:t) = conc(h, matrot (rotl t)) -}
 
+rotl :: [[a]]->[[a]]
+rotl = transpose . map reverse
+
 matrot :: Eq a => [[a]] -> [a]
-matrot = either nil (conc.(id >< matrot).(id><rotl)).outList
+matrot = either nil (conc.(id >< matrot).(id><rotl)) .outList
--- cp2324t/Q2.hs
+++ cp2324t/Q2.hs
@@ -1,6 +1,22 @@
 module Q2 where
+import Cp
+import List
 
+
+{- Versao PointWise
 reverseByPredicate :: (a -> Bool) -> [a] -> [a]
 reverseByPredicate p [] = []
 reverseByPredicate p (h:t)  |p h = reverseByPredicate p t ++ [h]
-                            |otherwise = h: reverseByPredicate p t
+                            |otherwise = h: reverseByPredicate p t -}
+
+
+{- reverseByPredicate :: (a -> Bool) -> [a] -> [a]
+reverseByPredicate p [] = []
+reverseByPredicate p list  = if (p.head) list then conc(split (reverseByPredicate p.tail) (singl.head) list)
+                            else cons(split head (reverseByPredicate p.tail) list) -}
+
+
+reverseByPredicate :: (a -> Bool) -> [a] -> [a]
+reverseByPredicate p = cataList (either nil (cond (p.p1)
+    (conc.split (reverseByPredicate p.p2) (singl.p1))
+    (cons.split p1 (reverseByPredicate p.p2))))
