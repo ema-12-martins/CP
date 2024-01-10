3c18dbd196583a2293351a4fb5963fe190ab12fe
a97455 <a97455@alunos.uminho.pt>
2024-01-10 20:10:09 +0000

    Q2 esta no mesmo estado que a Q1, mas ja funciona

--- cp2324t/Q2.hs
+++ cp2324t/Q2.hs
@@ -8,15 +8,9 @@ reverseByPredicate :: (a -> Bool) -> [a] -> [a]
 reverseByPredicate p [] = []
 reverseByPredicate p (h:t)  |p h = reverseByPredicate p t ++ [h]
                             |otherwise = h: reverseByPredicate p t -}
-
-
-{- reverseByPredicate :: (a -> Bool) -> [a] -> [a]
-reverseByPredicate p [] = []
-reverseByPredicate p list  = if (p.head) list then conc(split (reverseByPredicate p.tail) (singl.head) list)
-                            else cons(split head (reverseByPredicate p.tail) list) -}
-
+                            
 
 reverseByPredicate :: (a -> Bool) -> [a] -> [a]
-reverseByPredicate p = cataList (either nil (cond (p.p1)
+reverseByPredicate p = either nil (cond (p.p1)
     (conc.split (reverseByPredicate p.p2) (singl.p1))
-    (cons.split p1 (reverseByPredicate p.p2))))
+    (cons.split p1 (reverseByPredicate p.p2))) .outList
