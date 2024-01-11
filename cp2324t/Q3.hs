16e4b34587edd6d798ffb1ba802a09aad10a8080
ema-12-martins <a97678@alunos.uminho.pt>
2024-01-11 18:49:25 +0000

    3 a dar o valor e nao o tuplo

--- cp2324t/Q3.hs
+++ cp2324t/Q3.hs
@@ -7,9 +7,9 @@ f' x i = wrapper . for loop init where
         loop (f,k,g) = (f + k,k * ((x ^ 2)/g),g+2)
         init = (zero,x,one) -}
 
+wrapper(a,_,_) = a
 
-snh' x = id . worker where
-        wrapper(a,_,_) = a
+snh' x = wrapper . worker where
         worker = for (loop x) (start x)
 
 loop x (f,k,g)= (f + k,k * ((x ^ 2)/((g+1)*(g+2))),g+2)
