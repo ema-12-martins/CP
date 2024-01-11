94856b785e0c18c187b643156d53205a237b3f86
ema-12-martins <a97678@alunos.uminho.pt>
2024-01-11 15:46:24 +0000

    Mesmo quase

--- cp2324t/Q3.hs
+++ cp2324t/Q3.hs
@@ -8,9 +8,9 @@ f' x i = wrapper . for loop init where
         init = (zero,x,one) -}
 
 
-snh' x = wrapper . worker where
+snh' x = id . worker where
         wrapper(a,_,_) = a
         worker = for (loop x) (start x)
 
-loop n (f,k,g)= (f + k,k * ((n ^ 2)/g),g+2)
-start n = (n,n,1)
+loop n (f,k,g)= (f + k,k * ((n ^ 2)/((g+2)*(g+3))),g+1)
+start n = (n,n,0)
