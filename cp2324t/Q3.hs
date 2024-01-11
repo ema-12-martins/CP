a9f726a503ef8810166171f4ff0277436b5366ac
ema-12-martins <a97678@alunos.uminho.pt>
2024-01-11 18:51:36 +0000

    comecar poinfree

--- cp2324t/Q3.hs
+++ cp2324t/Q3.hs
@@ -2,12 +2,15 @@ import Data.List
 import Cp
 import Nat
 
-{- wrapper(a,_,_) = a
+------- Funcao necessaria para ambas as versoes------------------------
+wrapper(a,_,_) = a
+
+------------------Tentativa de sersao pointfree-------------------------
+
 f' x i = wrapper . for loop init where
         loop (f,k,g) = (f + k,k * ((x ^ 2)/g),g+2)
-        init = (zero,x,one) -}
-
-wrapper(a,_,_) = a
+        init = (zero,x,one) 
+----------------- Versao pointwise a funcionar--------------------------------
 
 snh' x = wrapper . worker where
         worker = for (loop x) (start x)
