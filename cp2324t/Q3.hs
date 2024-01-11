21f8837e48328040063318c7f2475be767a29d72
ema-12-martins <a97678@alunos.uminho.pt>
2024-01-11 13:07:40 +0000

    Coisas mal feitas mas melhores

--- cp2324t/Q3.hs
+++ cp2324t/Q3.hs
@@ -1,50 +1,16 @@
 import Data.List
 import Cp
 import Nat
-{-
-O do fatorial ficava assim entao:
 
-int fac(int n) {
-    int x=1; 
-    int y=1;
-    
-    int j;
-    for (j=1;j<n+1;j++) {
-        y=x*y;
-        x=x+1;
-        }
-    return y;
-};
--}
+{- wrapper(a,_,_) = a
+f' x i = wrapper . for loop init where
+        loop (f,k,g) = (f + k,k * ((x ^ 2)/g),g+2)
+        init = (zero,x,one) -}
 
-fibMeu = p2 . (for g (1, 1)) where g (x, y ) = (x + 1, x * y )
 
-{-
-Em C, o programa ficava assim:
+snh' x = wrapper . worker where
+        wrapper(a,_,_) = a
+        worker = for (loop x) (start x)
 
-double seno_hiperbolico(double x, int n) {
-    int x=0; 
-    int y=1;
-    int i;
-    for (i = 0; i < n; i++) {
-        y=(pow(x, 2 * i + 1) / fatorial(2 * i + 1))*y;
-        x=x+1;
-    }
-    return senh;
-}
--}
-
-shMeu = p2 . (for g (0, 1)) where g (x, y) = (x + 1, (pow(x, 2 * i + 1) / fatorial(2 * i + 1)) + y)
-
-{-
-Em C, o programa ficava assim:
-
-double seno_hiperbolico(double x, int n) {
-    double senh = 0;
-    int i;
-    for (i = 0; i < n; i++) {
-        senh += (pow(x, 2 * i + 1) / fatorial(2 * i + 1));
-    }
-    return senh;
-}
--}
+loop n (f,k,g)= (f + k,k * ((n ^ 2)/g),g+2)
+start n = (n,n,1)
