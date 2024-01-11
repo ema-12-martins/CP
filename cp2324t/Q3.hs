0626b965958fc8c34c7337b9c45fca47ef1aa787
ema-12-martins <a97678@alunos.uminho.pt>
2024-01-11 19:00:07 +0000

    dao as duas versoes da q3

--- cp2324t/Q3.hs
+++ cp2324t/Q3.hs
@@ -5,11 +5,11 @@ import Nat
 ------- Funcao necessaria para ambas as versoes------------------------
 wrapper(a,_,_) = a
 
-------------------Tentativa de sersao pointfree-------------------------
+--------------Versao pointfree a funcionar (ou o mais perto de ser)----------------
 
-f' x i = wrapper . for loop init where
-        loop (f,k,g) = (f + k,k * ((x ^ 2)/g),g+2)
-        init = (zero,x,one) 
+snh2 x = wrapper . for loop init where
+        loop (f,k,g)= (f + k,k * ((x ^ 2)/((g+1)*(g+2))),g+2)
+        init = (x,(x ^ 3)/6,3)
 ----------------- Versao pointwise a funcionar--------------------------------
 
 snh' x = wrapper . worker where
