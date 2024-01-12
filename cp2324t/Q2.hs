c24904121267b19260e42a6d71dc41c7a78867ba
a97455 <a97455@alunos.uminho.pt>
2024-01-12 16:08:04 +0000

    q2 final e q3 com tipos

--- cp2324t/Q2.hs
+++ cp2324t/Q2.hs
@@ -2,21 +2,22 @@ module Q2 where
 import Cp
 import List
 
+
 -- Predicado para vogais
 isVowel :: Char -> Bool
 isVowel c = c `elem` "aeiouAEIOU"
 
+
 reverseByPredicate :: (a -> Bool) -> [a] -> [a]
 reverseByPredicate p = rebuildList p.(id><filter p).split id id
 
-{- 
-rebuildList :: (a -> Bool) -> ([a],[a]) -> [a]
-rebuildList _ ([],_) = []
-rebuildList p (list,listPredicado)
-        | p (head list) = cons (last listPredicado,rebuildList p (tail list,init listPredicado))
-        | otherwise = cons (head list,rebuildList p (tail list,listPredicado)) -}
+
+myOutList :: ([a],[a]) -> Either () ([a], [a])
+myOutList ([],_) = i1 ()
+myOutList (list1,list2) = i2(list1,list2)
+
 
 rebuildList :: (a -> Bool) -> ([a], [a]) -> [a]
-rebuildList p = cond (p.head.p1)
+rebuildList p = either nil (cond (p.head.p1)
         (cons.(last><rebuildList p.(tail><init)).split p2 id)
-        (cons.(head><rebuildList p.(tail><id)).split p1 id)
+        (cons.(head><rebuildList p.(tail><id)).split p1 id)). myOutList
--- cp2324t/Q3.hs
+++ cp2324t/Q3.hs
@@ -3,17 +3,21 @@ import Cp
 import Nat
 
 ------- Funcao necessaria para ambas as versoes------------------------
+wrapper :: (a, b, c) -> a
 wrapper(a,_,_) = a
 
 --------------Versao pointfree a funcionar (ou o mais perto de ser)----------------
-
+snh2 :: (Integral a, Fractional c) => c -> a -> c
 snh2 x = wrapper . for loop init where
         loop (f,k,g)= (f + k,k * ((x ^ 2)/((g+1)*(g+2))),g+2)
         init = (x,(x ^ 3)/6,3)
------------------ Versao pointwise a funcionar--------------------------------
 
+----------------- Versao pointwise a funcionar--------------------------------
+snh' :: (Integral a, Fractional c) => c -> a -> c
 snh' x = wrapper . worker where
         worker = for (loop x) (start x)
 
+loop :: Fractional c => c -> (c, c, c) -> (c, c, c)
 loop x (f,k,g)= (f + k,k * ((x ^ 2)/((g+1)*(g+2))),g+2)
+start :: (Fractional b, Num c) => b -> (b, b, c)
 start x = (x,(x ^ 3)/6,3)
