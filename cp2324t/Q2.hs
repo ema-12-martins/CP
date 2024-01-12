2449cf97e4439482281f94f548004160dad446c7
a97455 <a97455@alunos.uminho.pt>
2024-01-12 16:13:04 +0000

    q2 final

--- cp2324t/Q2.hs
+++ cp2324t/Q2.hs
@@ -12,12 +12,12 @@ reverseByPredicate :: (a -> Bool) -> [a] -> [a]
 reverseByPredicate p = rebuildList p.(id><filter p).split id id
 
 
-myOutList :: ([a],[a]) -> Either () ([a], [a])
-myOutList ([],_) = i1 ()
-myOutList (list1,list2) = i2(list1,list2)
+outTupleList :: ([a],[a]) -> Either () ([a], [a])
+outTupleList ([],_) = i1 ()
+outTupleList (list1,list2) = i2(list1,list2)
 
 
 rebuildList :: (a -> Bool) -> ([a], [a]) -> [a]
 rebuildList p = either nil (cond (p.head.p1)
         (cons.(last><rebuildList p.(tail><init)).split p2 id)
-        (cons.(head><rebuildList p.(tail><id)).split p1 id)). myOutList
+        (cons.(head><rebuildList p.(tail><id)).split p1 id)). outTupleList
